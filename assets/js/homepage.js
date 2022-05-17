4af58d977edd4fd9e9e851e101994974d3f00889
Olive <olivelliott48@gmail.com>
2022-05-17 17:06:47 -0400

    Create form submit handler for searching git

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username")
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
     fetch(apiUrl).then(function(response) {
@@ -7,5 +10,15 @@ var getUserRepos = function(user) {
     });
 }
 
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a username")
+    }
+}
 
-getUserRepos("olivelliott");
+userFormEl.addEventListener("submit", formSubmitHandler);
