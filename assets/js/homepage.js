84bc038fb0de6d7ece4a38bdb6977eb8e691b7b7
Olive <olivelliott48@gmail.com>
2022-05-19 17:04:59 -0400

    Create fetch request for github repos on index.html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,7 +9,7 @@ var getUserRepos = function(user) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayRepos(data, user);
-            });    
+            });
         } else {
             alert("Error: GitHub User Not Found");
         }
---
+++ assets/js/single.js
@@ -0,0 +1,14 @@
+var getRepoIssues = function(repo) {
+    var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            })
+        } else {
+            alert("There was a problem with your request!");
+        }
+    });
+}
+
+getRepoIssues("facebook/react");
