3f9c217e7045ea9bf5e938f418e974dd12091020
Olive <olivelliott48@gmail.com>
2022-05-19 22:32:36 -0400

    Add extra message for repos with more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
+var repoNameEl = document.querySelector('#repo-name');
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
 
 var getRepoIssues = function(repo) {
     var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
@@ -6,7 +8,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
-            })
+                if (response.headers.get('Link')) {
+                    displayWarning(repo);
+                }
+            });
         } else {
             alert("There was a problem with your request!");
         }
@@ -23,18 +28,29 @@ var displayIssues = function(issues) {
         issueEl.classList = "list-item flex-row justify-space-between align-center";
         issueEl.setAttribute("href", issues[i].html_url);
         issueEl.setAttribute("target", "_blank");
-
+    
         var titleEl = document.createElement('span');
         titleEl.textContent = issues[i].title;
         issueEl.appendChild(titleEl);
 
         var typeEl = document.createElement('span');
         if (issues[i].pull_request) {
-            typeEl.textContent = '(Pull request)'
+            typeEl.textContent = '(Pull request)';
         } else {
             typeEl.textContent = '(Issue)';
-        } issueEl.appendChild(typeEl);
-    } issueContainerEl.appendChild(issueEl);
+        } 
+        issueEl.appendChild(typeEl);
+        issueContainerEl.appendChild(issueEl);
+    }
+};
 
-}
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit";
+    var linkEl = document.createElement('a');
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+    linkEl.setAttribute('target', '_blank');
+    limitWarningEl.appendChild(linkEl);
+};
+
 getRepoIssues("facebook/react");
